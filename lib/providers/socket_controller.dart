05ec29403dd3b8f0a588249688f8e684fce68506
JuHyerin <jhr67370719@gmail.com>
2022-11-10 15:25:14 +0900

    clean

--- lib/providers/socket_controller.dart
+++ lib/providers/socket_controller.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter_mts/models/kis_socket_request_param.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
@@ -9,41 +8,36 @@ class SocketController {
   late final String serviceCd;
   late Map<String, WebSocketChannel> channelMap = {};
 
-  SocketController({required this.serviceCd, required List<String> keys}) {
-    KisSocketRequestHeader header = KisSocketRequestHeader(
-        approvalKey: '93fba692-6041-4e19-9939-07bdae320979',
-        custType: 'P',
-        trType: 1
-    );
-
+  SocketController({
+    required this.serviceCd,
+    required List<String> keys,
+  }) {
     for(var key in keys){
-      KisSocketRequestParam params = KisSocketRequestParam(
-        header: header,
-        body: KisSocketRequestBody(trId: serviceCd, trKey: key)
-      );
       addChannel(key);
-      addData(key, params);
     }
   }
 
   Uri buildSocketUri() => Uri.parse('$serverUrl$serviceCd');
 
+  Parameter buildParams(String trKey) {
+    return KisSocketRequestParam(
+      header: KisSocketRequestHeader(
+        approvalKey: '93fba692-6041-4e19-9939-07bdae320979',
+        custType: 'P',
+        trType: 1
+      ),
+      body: KisSocketRequestBody(trId: serviceCd, trKey: trKey)
+    );
+  }
+
   void addChannel(String key) {
     WebSocketChannel channel = WebSocketChannel.connect(buildSocketUri());
     channelMap[key] = channel;
+    addData(key);
   }
 
-  void addData(String key, Parameter data){
-    // KisSocketRequestParam data = KisSocketRequestParam(
-    //   header: KisSocketRequestHeader(
-    //       approvalKey: '93fba692-6041-4e19-9939-07bdae320979',
-    //       custType: 'P',
-    //       trType: 1
-    //   ),
-    //   body: KisSocketRequestBody(trId: serviceCd, trKey: key)
-    // );
-
-    channelMap[key]!.sink.add(jsonEncode(data.toJson()));
+  void addData(String key){
+    channelMap[key]!.sink.add(jsonEncode(buildParams(key).toJson()));
   }
 
   List<MapEntry<String, WebSocketChannel>> mapToList() {
--- lib/screens/controller_test_screen.dart
+++ lib/screens/controller_test_screen.dart
@@ -1,9 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_mts/models/kis_socket_request_param.dart';
 import 'package:flutter_mts/providers/socket_controller.dart';
-import 'package:flutter_mts/providers/stock_socket.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 List<String> trKeys = ['005930', '033180'];
@@ -16,19 +12,15 @@ class ControllerTestScreen extends StatefulWidget {
 
 class _ControllerTestScreenState extends State<ControllerTestScreen> {
   late final SocketController socketController;
-  late List<MapEntry<String, WebSocketChannel>> list;
 
   @override
   void initState() {
     super.initState();
     socketController = SocketController(
-        serviceCd: 'H0STASP0',
-        keys: trKeys
+      serviceCd: 'H0STASP0',
+      keys: trKeys,
     );
-    list = socketController.mapToList();
-
   }
-
   @override
   Widget build(BuildContext context) {
 
@@ -39,39 +31,30 @@ class _ControllerTestScreenState extends State<ControllerTestScreen> {
         ),
         body: Center(
           child: Column(
-            children: list.map((e) =>
-              StreamBuilder(
-                stream: e.value.stream,
-                builder: (context, snapshot) {
-                  print(snapshot.toString());
-                  return Center(
-                    child: Text('${snapshot.data}'),
-                  );
-                },
-              )).toList(),
+            children: socketController
+                .mapToList()
+                .map((e) => StockStreamWidget(channelSet: e))
+                .toList(),
           ),
         )
     );
   }
 }
-// class StockStreamWidget extends StatelessWidget {
-//   final MapEntry<String, WebSocketChannel> channelSet;
-//   StockStreamWidget({required this.channelSet});
-//
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     print('${channelSet.key}>> ${channelSet.value}');
-//
-//     return StreamBuilder(
-//       stream: channelSet.value.stream,
-//       builder: (context2, snapshot) {
-//         print(snapshot.toString());
-//         return Center(
-//           child: Text('${snapshot.data}'),
-//         );
-//       },
-//     );
-//   }
-//
-// }
+class StockStreamWidget extends StatelessWidget {
+  final MapEntry<String, WebSocketChannel> channelSet;
+  StockStreamWidget({required this.channelSet});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: channelSet.value.stream,
+      builder: (context, snapshot) {
+        print(snapshot.toString());
+        return Center(
+          child: Text('${snapshot.data}'),
+        );
+      },
+    );
+  }
+
+}
